c03e16c14b9fc6c36875bd67863c3d0db688e772
elguerdi <ibtissamelguerdi90@gmail.com>
2024-11-07 15:16:40 +0100

    Premier commit de mon portfolio

---
+++ src/data/MenuData.js
@@ -0,0 +1,26 @@
+export const links = [
+  { href: "#skills", text: "Skills" },
+  { href: "#tools", text: "Tools" },
+  { href: "#about", text: "About" },
+  { href: "#portafolio", text: "Projects" },
+];
+export const about = [
+  { href: "/", text: "Home" },
+  { href: "#about", text: "About" },
+  { href: "/portfolio", text: "Projects" },
+];
+export const portfolio = [
+  { href: "/", text: "Home", target: "_self" },
+  { href: "/about", text: "About Me", target: "_self" },
+  { href: "https://github.com/Elguerdi", text: "My GitHub", target: "_blank" },
+];
+export const blog = [
+  { href: "/", text: "Home", target: "_self" },
+  { href: "/about", text: "About", target: "_self" },
+  { href: "/portfolio", text: "Portfolio", target: "_self" },
+  {
+    href: "https://sofidev.medium.com/",
+    text: "Medium blog",
+    target: "_blank",
+  },
+];
---
+++ src/data/portfolioData.js
@@ -0,0 +1,63 @@
+ const portafolioData = [
+  {
+    imgSrc: "../img/spendsmart.png",
+    titulo: "SpendSmart",
+    skills: ["Flutter", "Swift","Firebase"],
+    descripcion:
+      "SpendSmart is a banking app designed to make financial management simple and efficient. It features a friendly chatbot for quick assistance and an invoice scanner for easy expense tracking, along with tools to set budgets and monitor spending.",
+    demoURL: "https://alura-flix-self.vercel.app/",
+    repoURL: "https://github.com/Elguerdi/SpendSmart",
+    anim:"fade-right"
+  },
+  {
+    imgSrc: "../img/php.png",
+    titulo: "Stock Management System",
+    skills: ["JavaScript", "PHP"],
+    descripcion: " Stock Management System is an intuitive tool for tracking inventory and managing stock levels efficiently. With features like real-time stock updates and low-quantity alerts, it simplifies inventory control for businesses. This project was a great way to build a practical solution that streamlines daily operations.",
+    demoURL: "https://alura-geek-ruddy.vercel.app/",
+    repoURL: "https://github.com/Elguerdi/Stock-Management-System",
+    anim:"fade-up"
+  },
+  {
+    imgSrc: "../img/ai.jpg",
+    titulo: "AI Image Generator",
+    skills: ["Flutter", "C"],
+    descripcion: "AI Image Generator is an innovative tool that uses artificial intelligence to create unique images from text prompts.it enables users to generate visuals quickly, whether for design, marketing, or personal projects. Building this tool allowed me to explore AI's potential in creative applications and make art generation accessible to anyone with a simple idea.",
+    demoURL: "https://react-org-delta.vercel.app/",
+    repoURL: "https://github.com/Elguerdi/AI-Image-Generator",
+    anim:"fade-left"
+  },
+  {
+    imgSrc: "../img/movies.webp",
+    titulo: "Movies App",
+    skills: ["Flutter", "C"],
+    descripcion: "Movies App is a sleek and user-friendly mobile app that allows users to explore and discover movies based on genres, ratings, and release dates. With real-time movie data integration, it provides detailed information on cast, plot summaries, and trailers. This project was a fun challenge, combining my love for movies with my passion for creating engaging, easy-to-use apps.",
+    demoURL: "https://sofidev-portfolio-astro-delta.vercel.app/",
+    repoURL: "https://github.com/Elguerdi/movies_app_flutter",
+    anim:"fade-right"
+  },
+  {
+    imgSrc: "../img/quiz.webp",
+    titulo: "Quiz App",
+    skills: ["Flutter", "Firebase"],
+    descripcion: "Quiz App is an interactive mobile application that allows users to take quizzes on various topics, track their scores, and challenge friends. Designed with a simple and engaging interface, it offers multiple-choice questions, timers, and instant feedback. This project was a great opportunity to combine fun and learning in a user-friendly way, creating an app that’s both educational and entertaining.",
+    demoURL: "https://sofidevo.github.io/eco-store-sass/",
+    repoURL: "https://github.com/Elguerdi/Quiz-app-with-Flutter-",
+    anim:"fade-up"
+  },
+  
+  
+];
+
+ const skillIcons = {
+  Flutter:"devicon:dart",
+  JavaScript: "skill-icons:javascript",
+  Swift: "devicon:swift",
+  Firebase: "devicon:firebase",
+  PHP: "devicon:php",
+  C: "skill-icons:cpp",
+  StyledComponents: "skill-icons:styledcomponents",
+  Hive: "skill-icons:hive",
+};
+
+export  {portafolioData,skillIcons}
